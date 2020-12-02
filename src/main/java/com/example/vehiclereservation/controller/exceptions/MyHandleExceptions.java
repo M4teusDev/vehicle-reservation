48d3b65d086297bf51577332bee8256e887bda45
Mateus Prado <mateus.silvaprado@gmail.com>
2020-12-02 00:42:18 -0300

    Commit 02/12

--- src/main/java/com/example/vehiclereservation/controller/exceptions/MyHandleExceptions.java
+++ src/main/java/com/example/vehiclereservation/controller/exceptions/MyHandleExceptions.java
@@ -3,12 +3,12 @@ package com.example.vehiclereservation.controller.exceptions;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
 
 @RestControllerAdvice
 public class MyHandleExceptions {
--- src/main/java/com/example/vehiclereservation/service/ServiceReservation.java
+++ src/main/java/com/example/vehiclereservation/service/ServiceReservation.java
@@ -19,7 +19,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-
 @Service
 public class ServiceReservation {
     
